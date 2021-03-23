1d6103c86603f33453fb9aa1c28a1117cd943e3e
taku horikoshi <taku@takus-mac.local>
2021-03-23 15:26:10 +0900

    fix 一覧ページにアクセスした場合、Taskが表示されること　のリファクタリング

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Task', type: :system do
   describe 'Task一覧' do
     context '正常系' do
+      let(:project) { create(:project) }
+      let(:task) { create(:task) }
       it '一覧ページにアクセスした場合、Taskが表示されること' do
         # TODO: ローカル変数ではなく let を使用してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
+        
         visit project_tasks_path(project)
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
