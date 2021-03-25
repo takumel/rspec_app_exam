4726b66dd72a765c5a81e0f38cf854e67a9b2acb
taku horikoshi <taku@takus-mac.local>
2021-03-26 03:55:45 +0900

    fix Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること の修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -13,12 +13,11 @@ RSpec.describe 'Task', type: :system do
         expect(current_path).to eq project_tasks_path(project)
       end
 
-      xit 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
+      it 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
         # FIXME: テストが失敗するので修正してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
         visit project_path(project)
         click_link 'View Todos'
+        switch_to_window(windows.last)
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
         expect(current_path).to eq project_tasks_path(project)
@@ -63,7 +62,7 @@ RSpec.describe 'Task', type: :system do
       let(:project) {create(:project)}
       let(:task) {create(:task)}
 
-      fit 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
+      it 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
         visit edit_project_task_path(project, task)
         fill_in 'Deadline', with: Time.current
