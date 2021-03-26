172ab3ae4e746f12a50208b2a77fc7d158530982
taku horikoshi <taku@takus-mac.local>
2021-03-26 19:14:37 +0900

    余計なスペースを削除

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe 'Task', type: :system do
         expect(page).to have_content(Time.current.strftime('%Y-%m-%d'))
         expect(current_path).to eq project_task_path(project, task)
       end
-      
+
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
         task = create(:task, :done)
