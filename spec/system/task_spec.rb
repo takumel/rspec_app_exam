34a011a8b27de46acd323623189ecaea0ead56f3
taku horikoshi <taku@takus-mac.local>
2021-03-26 18:10:18 +0900

    fix Taskが削除されること fit→it

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -91,7 +91,7 @@ RSpec.describe 'Task', type: :system do
     context '正常系' do
     let!(:task) { create(:task) }
       # FIXME: テストが失敗するので修正してください
-      fit 'Taskが削除されること' do
+      it 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
